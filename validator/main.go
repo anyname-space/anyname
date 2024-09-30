e21820a1c3b1e4eeae58d4776d49c4df4597445e
mek <maulanaaghni.contact@gmail.com>
2024-09-30 15:52:33 +0700

    add validator: name, category

---
+++ validator/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	// Validasi nama-nama JSON di folder data/names/
+	invalidFiles, err := ValidateNames()
+	if err != nil {
+		fmt.Println("Error:", err)
+		os.Exit(1)
+	}
+
+	// Output hasil validasi nama
+	if len(invalidFiles) > 0 {
+		fmt.Println("Invalid name files found:")
+		for _, file := range invalidFiles {
+			fmt.Println(file)
+		}
+		// Output untuk GitHub Actions menangkap hasil
+		fmt.Printf("::set-output name=invalidFiles::%s\n", strings.Join(invalidFiles, ", "))
+		os.Exit(1)
+	} else {
+		fmt.Println("All name files are valid.")
+	}
+
+	// Validasi kategori JSON di data/categories.json
+	categoryFilePath := "../data/category.json" // Sesuaikan path relatif ke file
+	err = ValidateCategory(categoryFilePath)
+	if err != nil {
+		fmt.Println("Error in category validation:", err)
+		os.Exit(1)
+	} else {
+		fmt.Println("Category file is valid.")
+	}
+}
